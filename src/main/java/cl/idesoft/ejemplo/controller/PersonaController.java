b0acaea2e55b7b955c14d4b938505c6550b78e80
German Munoz Soto <gmunoz@transbank.cl>
2020-11-11 19:32:17 -0300

    Integracion con base de datos en memoria

--- src/main/java/cl/idesoft/ejemplo/controller/PersonaController.java
+++ src/main/java/cl/idesoft/ejemplo/controller/PersonaController.java
@@ -1,7 +1,9 @@
 package cl.idesoft.ejemplo.controller;
 
 import cl.idesoft.ejemplo.dto.Persona;
+import cl.idesoft.ejemplo.service.IPersonaService;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,29 +13,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/v1")
 public class PersonaController {
 
+    @Autowired
+    private IPersonaService personaService;
+
     @PostMapping(path = "/persona", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> crearPersona(@RequestBody Persona persona) {
+        personaService.insertarPersona(persona);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping(path = "/persona/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Persona> obtenerPersona(@PathVariable(value = "id") Integer id) {
-        Persona persona = new Persona();
-        persona.setId(id);
-        persona.setNombre("Germán");
-        persona.setApellido("Muñoz");
-        persona.setEdad("30");
+        Persona persona = personaService.obtenerPersona(id);
         return ResponseEntity.ok(persona);
     }
 
     @PutMapping(path = "/persona", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Persona> actualizarPersona(@RequestBody Persona persona) {
-        return ResponseEntity.ok(persona);
+    public ResponseEntity<Void> actualizarPersona(@RequestBody Persona persona) {
+        personaService.actualizarPersona(persona);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping(path = "/persona/{id}")
     public ResponseEntity<Void> eliminarPersona(@PathVariable(value = "id") Integer id) {
-        System.out.println(id);
+        personaService.eliminarPersona(id);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/cl/idesoft/ejemplo/dto/Persona.java
+++ src/main/java/cl/idesoft/ejemplo/dto/Persona.java
@@ -12,6 +12,6 @@ public class Persona {
     private int id;
     private String nombre;
     private String apellido;
-    private String edad;
+    private int edad;
 
 }
